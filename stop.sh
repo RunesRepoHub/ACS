78513dd6fb12bf0eefb9b215a002b1d7578d1413
rune <christian.rune1998@gmail.com>
2023-11-07 20:06:56 +0100

    Added stop docker download containers

--- stop.sh
+++ stop.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Stop and remove any docker with the image plexinc/pms-docker
+docker stop $(docker ps -a -q --filter="ancestor=plexinc/pms-docker")
+docker rm $(docker ps -a -q --filter="ancestor=plexinc/pms-docker")
+
 # Stop and remove the dockers
 docker stop plex jackett radarr sonarr tautulli deluge ombi
 docker rm plex jackett radarr sonarr tautulli deluge ombi
