6a78fba15861ad75dee3f4eb0d0e5b676a729ccc
rune <christian.rune1998@gmail.com>
2023-11-07 21:07:43 +0100

    Hotfix

--- stop.sh
+++ stop.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# Stop and remove any docker with the image plexinc/pms-docker
-docker stop $(docker ps -a -q --filter="mikenye/youtube-dl")
-docker rm $(docker ps -a -q --filter="mikenye/youtube-dl")
+# Stop and remove any docker with the image mikenye/youtube-dl
+container_count=$(docker ps -a --filter="ancestor=mikenye/youtube-dl" --format "{{.ID}}" | wc -l)
+if [[ $container_count -gt 0 ]]; then
+    docker stop $(docker ps -a --filter="ancestor=mikenye/youtube-dl" --format "{{.ID}}")
+    docker rm $(docker ps -a --filter="ancestor=mikenye/youtube-dl" --format "{{.ID}}")
+fi
 
 # Stop and remove the dockers
 docker stop plex jackett radarr sonarr tautulli deluge ombi
