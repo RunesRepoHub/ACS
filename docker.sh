141b32054591bd4d869a5bf6f086d3d64fe16d4d
rune <christian.rune1998@gmail.com>
2023-11-07 05:37:22 +0100

    Hotfix

--- docker.sh
+++ docker.sh
@@ -31,10 +31,7 @@ install_docker_debian() {
     sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
     # Add the repository to Apt sources
-    echo \
-      "deb [arch=\"$(dpkg --print-architecture)\" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-      \"$(. /etc/os-release && echo \"$VERSION_CODENAME\")\" stable" | \
-      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker.gpg] https://download.docker.com/linux/debian bookworm stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
     apt-get update > /dev/null 2>&1
     apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin > /dev/null 2>&1
