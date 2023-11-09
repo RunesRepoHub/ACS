3117afee9a6cc504b1910122ca1216c955d6cb4d
rune <christian.rune1998@gmail.com>
2023-11-09 21:08:05 +0100

    Fixed uninstall

--- Scripts/uninstall.sh
+++ Scripts/uninstall.sh
@@ -28,30 +28,21 @@ echo -e "${Red}n = Delete plex media folder${NC}"
 # Prompt the user for a yes/no answer
 read -p "Are you sure? (y/n): " answer
 
-
-# Stop and remove any docker with the image mikenye/youtube-dl
-echo -e "${Red}Stopping any and all mikenye/youtube-dl dockers${NC}"
-container_count=$(docker ps -a --filter="ancestor=mikenye/youtube-dl" --format "{{.ID}}" | wc -l)
-for container_id in $(docker ps -a --filter="ancestor=mikenye/youtube-dl" --format "{{.ID}}"); do
-    docker stop $container_id
-done
-echo -e "${Green}All mikenye/youtube-dl dockers have been stopped and removed${NC}"
-
-
-# Stop and remove the dockers
-echo -e "${Red}Stopping plex, jackett, radarr, sonarr, tautulli, deluge and ombi${NC}"
-docker stop jackett radarr sonarr tautulli deluge ombi > /dev/null 2>&1
-docker rm jackett radarr sonarr tautulli deluge ombi > /dev/null 2>&1
-echo -e "${Green}All plex, jackett, radarr, sonarr, tautulli, deluge and ombi dockers have been stopped${NC}"
-
-
-
 # Check the user's response
 if [[ $answer == "y" ]]; then
     # User answered "yes"
-    # run docker stop all
-    bash ~/Auto-YT-DL/stop.sh
-    sleep 2
+    # Stop and remove any docker with the image mikenye/youtube-dl
+    echo -e "${Red}Stopping any and all mikenye/youtube-dl dockers${NC}"
+    container_count=$(docker ps -a --filter="ancestor=mikenye/youtube-dl" --format "{{.ID}}" | wc -l)
+    for container_id in $(docker ps -a --filter="ancestor=mikenye/youtube-dl" --format "{{.ID}}"); do
+        docker stop $container_id
+    done
+    echo -e "${Green}All mikenye/youtube-dl dockers have been stopped and removed${NC}"
+    # Stop and remove the dockers
+    echo -e "${Red}Stopping jackett, radarr, sonarr, tautulli, deluge and ombi${NC}"
+    docker stop jackett radarr sonarr tautulli deluge ombi 
+    docker rm jackett radarr sonarr tautulli deluge ombi
+    echo -e "${Green}All jackett, radarr, sonarr, tautulli, deluge and ombi dockers have been stopped${NC}"
     # remove all folders and files
     rm -rf ~/Auto-YT-DL 
     echo -e "${Green}All folders and files has been removed except the plex media folder, all dockers has been stopped${NC}"
@@ -61,14 +52,24 @@ if [[ $answer == "y" ]]; then
     
 elif [[ $answer == "n" ]]; then
     # User answered "no"
-    # run docker stop all
-    bash ~/Auto-YT-DL/Scripts/stop-remove.sh
-    sleep 2
+        # Stop and remove any docker with the image mikenye/youtube-dl
+    echo -e "${Red}Stopping any and all mikenye/youtube-dl dockers${NC}"
+    container_count=$(docker ps -a --filter="ancestor=mikenye/youtube-dl" --format "{{.ID}}" | wc -l)
+    for container_id in $(docker ps -a --filter="ancestor=mikenye/youtube-dl" --format "{{.ID}}"); do
+        docker stop $container_id
+    done
+    echo -e "${Green}All mikenye/youtube-dl dockers have been stopped and removed${NC}"
+    # Stop and remove the dockers
+    echo -e "${Red}Stopping plex, jackett, radarr, sonarr, tautulli, deluge and ombi${NC}"
+    docker stop plex jackett radarr sonarr tautulli deluge ombi 
+    docker rm plex jackett radarr sonarr tautulli deluge ombi
+    echo -e "${Green}All plex, jackett, radarr, sonarr, tautulli, deluge and ombi dockers have been stopped${NC}"
     # remove all folders and files
     rm -rf ~/Auto-YT-DL  ~/plex
     echo -e "${Green}All folders and files has been removed, all dockers has been stopped${NC}"
-    docker stop plex
-    docker rm plex
+
+    # Remove the line from the crontab file
+    sudo sed -i '/Auto-YT-DL\/Scripts\/automated-check.sh/d' /etc/crontab
 else
     # User entered an invalid response
     echo -e "${Red}Error code: 400${NC}"
