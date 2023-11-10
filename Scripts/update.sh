90a3dbd96989f03e109372609d1efea61cd120d0
rune <christian.rune1998@gmail.com>
2023-11-10 08:44:37 +0100

    Hotfix

--- Scripts/update.sh
+++ Scripts/update.sh
@@ -3,10 +3,10 @@ Dev="Production"
 export Dev=$Dev
 
 # Update update.sh
-echo -e "${Purple}Updating update.sh...${NC}"
-rm ~/Auto-YT-DL/Scripts/update.sh
+echo -e "${Purple}Updating download-update.sh...${NC}"
+rm ~/Auto-YT-DL/Scripts/download-update.sh
 sleep 1
-curl -s -o ~/Auto-YT-DL/Scripts/update.sh https://raw.githubusercontent.com/RunesRepoHub/YT-Plex/$Dev/Scripts/update.sh > /dev/null
-echo -e "${Green}update.sh has been updated.${NC}"
+curl -s -o ~/Auto-YT-DL/Scripts/download-update.sh https://raw.githubusercontent.com/RunesRepoHub/YT-Plex/$Dev/Scripts/download-update.sh > /dev/null
+echo -e "${Green}download-update.sh has been updated.${NC}"
 
 bash ~/Auto-YT-DL/Scripts/download-update.sh
