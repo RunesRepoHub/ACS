26acc478cd71329cef863cb774aaf2b40d28954e
rune <christian.rune1998@gmail.com>
2023-11-11 15:24:27 +0100

    Hotfix

--- Scripts/update.sh
+++ Scripts/update.sh
@@ -5,7 +5,7 @@ source ~/Auto-YT-DL/Scripts/Core.sh
 echo -e "$UPDATE_AUTO_YT_DL"
 rm $ROOT_FOLDER/$UPDATE_DOWNLOAD
 sleep 1
-curl -s -o $ROOT_FOLDER/$UPDATE_DOWNLOAD $GIHUB_LINK/$GITHUB_FOLDER/$UPDATE_DOWNLOAD > /dev/null
+curl -s -o $ROOT_FOLDER/$UPDATE_DOWNLOAD https://raw.githubusercontent.com/RunesRepoHub/YT-Plex/Production/Scripts/download-update.sh > /dev/null
 echo -e "$UPDATE_AUTO_YT_DL_COMPLETED"
 
 bash $ROOT_FOLDER/$UPDATE_DOWNLOAD
