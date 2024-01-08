847c61bbad3a47b20ff08cfee3a77f90147e61f0
rune <christian.rune1998@gmail.com>
2024-01-08 12:59:47 +0100

    Updated to new update system

--- ACSF-Scripts/update.sh
+++ ACSF-Scripts/update.sh
@@ -2,18 +2,8 @@
 
 source ~/ACS/ACSF-Scripts/Core.sh
 
-#############
-### TO-DO ###
-#############
-### Check paths
-### Changes download link and methode 
-### Download the whole repo and use git clone 
-
 # Update update.sh
 echo -e "${Purple}Updating download-update.sh...${NC}"
-rm $ROOT_FOLDER/$UPDATE_DOWNLOAD
-sleep 1
-curl -s -o $ROOT_FOLDER/$UPDATE_DOWNLOAD $GIHUB_LINK/$GITHUB_FOLDER/$UPDATE_DOWNLOAD > /dev/null
+cd ~/ACS
+git pull
 echo -e "${Green}Download-update.sh has been updated${NC}"
-
-bash $ROOT_FOLDER/$UPDATE_DOWNLOAD
