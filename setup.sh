8e928ee4ee78c71aa87c6579775914e0d9445eec
rune <christian.rune1998@gmail.com>
2024-01-17 13:46:41 +0100

    Removed chmod

--- setup.sh
+++ setup.sh
@@ -98,11 +98,6 @@ else
 fi
 echo -e "${Green}Folders created${NC}"
 
-sudo chmod 777 $MOVIES
-sudo chmod 777 $SHOWS
-sudo chmod 777 $MEDIA_DOWNLOAD
-sudo chmod 777 $DOWNLOAD_COMPLETED
-
 # Take user input and save it to a file
 echo -e "${Purple}Enter the maximum number of containers to run for the youtube downloader${NC}"
 echo -e "${Yellow}These containers are used to download videos${NC}"
